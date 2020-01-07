552143567c8b7a465937541e8b8fd7bc7ce2e3dd
Franco Canedo <francanedo1@hotmail.com>
2020-01-07 17:28:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -40,7 +40,6 @@ def update_counting_hash(hash, key)
   # if the provided key is present, increment its value by 1
   if hash[key]
     return hash[key] += 1
-     
   else 
     return hash[key] = 1 
     
